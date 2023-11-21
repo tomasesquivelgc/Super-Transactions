fe0a0b6079cff5e1f1b98624c1ccfbdfb2ee6d74
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-11-21 12:19:38 -0300

    add categorization table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_234800) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_150048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_234800) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categorizations", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "transaction_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categorizations_on_category_id"
+    t.index ["transaction_id"], name: "index_categorizations_on_transaction_id"
   end
 
   create_table "transactions", force: :cascade do |t|
@@ -42,5 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_234800) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categorizations", "categories"
+  add_foreign_key "categorizations", "transactions"
   add_foreign_key "transactions", "users"
 end
