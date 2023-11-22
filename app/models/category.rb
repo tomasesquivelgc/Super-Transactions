adc4383aa9d393b57ef5229e9c4454b1bc6c144c
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-11-22 04:26:44 -0300

    correctly save categorization relationship

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :categorizations
-  has_many :categorized_transactions, through: :categorizations
+  has_many :transactions, through: :categorizations
 
   validates :name, presence: true
   validates :image, presence: true
