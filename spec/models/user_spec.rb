5f501d62b4aee36f2b0c736e08589dd949e82f93
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-11-23 15:06:45 -0300

    write tests for models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    user = User.new(name: "Example User", email: "user@example.com", password: "password")
+    expect(user).to be_valid
+  end
+
+  it "is not valid without a name" do
+    user = User.new(name: nil, email: "user@example.com", password: "password")
+    expect(user).not_to be_valid
+  end
+
+  it "has many transactions" do
+    association = described_class.reflect_on_association(:transactions)
+    expect(association.macro).to eq(:has_many)
+    expect(association.options[:dependent]).to eq(:destroy)
+  end
+
+  it "includes Devise modules" do
+    expect(described_class.devise_modules).to include(:database_authenticatable, :registerable, :recoverable, :rememberable, :validatable)
+  end
+
+  it "is database authenticatable" do
+    expect(described_class.devise_modules).to include(:database_authenticatable)
+  end
+
+  it "is registerable" do
+    expect(described_class.devise_modules).to include(:registerable)
+  end
+
+  it "is recoverable" do
+    expect(described_class.devise_modules).to include(:recoverable)
+  end
+
+  it "is rememberable" do
+    expect(described_class.devise_modules).to include(:rememberable)
+  end
+
+  it "is validatable" do
+    expect(described_class.devise_modules).to include(:validatable)
+  end
 end
