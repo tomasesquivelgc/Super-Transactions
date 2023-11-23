b2b3c9f8d28ce1ab194051b59c94db0ac4d655f7
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-11-22 21:05:52 -0300

    add total amount to categories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,13 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, except: [:splash]
+  helper_method :calculate_total_amount
   def index
     @categories = Category.all.includes(:categorizations)
   end
 
   def splash; end
+
+  def calculate_total_amount(category)
+    category.categorizations.map { |categorization| categorization.categorized_transaction.amount.to_f }.sum
+  end
 end
