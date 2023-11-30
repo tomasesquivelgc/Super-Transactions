7539f490ec1c0aa66a669327396e95cf96b22727
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-11-29 21:53:51 -0300

    add active record image to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   has_many :categorizations
   has_many :transactions, through: :categorizations
+  has_one_attached :image
 
   validates :name, presence: true
   validates :image, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_203621) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_30_004513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,7 +44,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_203621) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
